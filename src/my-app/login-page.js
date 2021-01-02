abc4601e53746c7a67a2a30798022a0775e195e7
pakkarakarisaibalaji <56920717+pakkarakarisaibalaji@users.noreply.github.com>
2021-01-02 02:35:02 -0800

    changes
    chnages

--- src/my-app/login-page.js
+++ src/my-app/login-page.js
@@ -8,16 +8,49 @@ class LoginPage extends PolymerElement {
   static get template() {
     return html`
       <style>
-        :host {
-          display: block;
+      :host {
+        display: block;
+        height: 100%;
+        color: var(--container-text, blue);
+        background-image: var(--container-background, "ins.jpg");
+        --container-background: url( '../src/assests/ins.jpg');
+      }
+        .card{
+          width:375px;
+          margin: 8em;
+          padding: 16px;
+          color: #757575;
+          border-radius: 5px;
+          background-color: #fff;
+          box-shadow: 0 2px 2px 0 rgba(0, 0, 0, 0.14), 0 1px 5px 0 rgba(0, 0, 0, 0.12), 0 3px 1px -2px rgba(0, 0, 0, 0.2);
+        }
+        h1{
+          margin: 16px 0;
+          color: #212121;
+          font-size: 22px;
+          padding-left: 7em;
+        }
+        .savings{
+        padding-left: 20em;
+          background:
+        }
+        .html{
+          background:#fff;
         }
       </style>
-      <h2>saibalaji [[prop1]]!</h2>
-      <div>Custom element</div>  
-      <paper-input label="Enter 1" value="{{king}}"></paper-input>
-      <paper-input label="Enter 2" value="{{king}}"></paper-input>
-      <paper-input label="Enter 3" value="[[prop1]]"></paper-input>
-    `;
+      <html>
+      
+      <div class="card">
+
+      <h1>Login</h1>
+      <h5>Customerid</h5>
+      <paper-input  value="{{username}}"></paper-input>
+      <h5>Password</h5>
+      <paper-input  value="{{password}}"></paper-input>
+    </div>
+  
+    </html>
+        `;
   }
   static get properties() {
     return {
@@ -25,9 +58,13 @@ class LoginPage extends PolymerElement {
         type: String,
         value: 'sai'
       },
-      king: {
+      username: {
         type: String,
-        value: 'saibalaji'
+        value: ''
+      },
+      password: {
+        type: String,
+        value: ''
       }
     };
   }
--- src/my-app/my-app.js
+++ src/my-app/my-app.js
@@ -19,14 +19,51 @@ class MyApp extends PolymerElement {
   static get template() {
     return html`
       <style>
-        :host {
-          display: block;
-        }
+      
+      :host {
+        display: block;
+        height: 100%;
+        
+      }
+      .card{
+        width:375px;
+        margin: 8em;
+        padding: 16px;
+        color: #757575;
+        border-radius: 5px;
+        background-color: #fff;
+        box-shadow: 0 2px 2px 0 rgba(0, 0, 0, 0.14), 0 1px 5px 0 rgba(0, 0, 0, 0.12), 0 3px 1px -2px rgba(0, 0, 0, 0.2);
+      }
+      h1{
+        margin: 16px 0;
+        color: #212121;
+        font-size: 22px;
+        padding-left: 7em;
+      }
+      h5{
+        margin: 16px 0;
+        color: #212121;
+        font-size: 12px;
+        
+      }
+      .savings{
+      padding-left: 20em;
+        background:
+      }
+
       </style>
-      <h2>saibalaji [[prop1]]!</h2>
-      <div>Custom element</div> 
-      <login-page></login-page> 
-   
+      
+      <div class="savings">
+      <div class="card">
+
+      <h1>Login</h1>
+      <h5>Customerid</h5>
+      <paper-input  value="{{username}}"></paper-input>
+      <h5>Password</h5>
+      <paper-input  value="{{password}}"></paper-input>
+    </div>
+    </div>
+  
     `;
   }
   static get properties() {
@@ -41,6 +78,7 @@ class MyApp extends PolymerElement {
       }
     };
   }
+  
 }
 
 window.customElements.define('my-app', MyApp);
