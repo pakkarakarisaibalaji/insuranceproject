3f3f8a8a82c6dae658294a2a571e85f363febc43
pakkarakarisaibalaji <56920717+pakkarakarisaibalaji@users.noreply.github.com>
2021-01-05 10:07:41 -0800

    commit
    commit

--- src/my-app/my-app.js
+++ src/my-app/my-app.js
@@ -1,12 +1,3 @@
-/**
- * @license
- * Copyright (c) 2016 The Polymer Project Authors. All rights reserved.
- * This code may only be used under the BSD style license found at http://polymer.github.io/LICENSE.txt
- * The complete set of authors may be found at http://polymer.github.io/AUTHORS.txt
- * The complete set of contributors may be found at http://polymer.github.io/CONTRIBUTORS.txt
- * Code distributed by Google as part of the polymer project is also
- * subject to an additional IP rights grant found at http://polymer.github.io/PATENTS.txt
- */
 
 import { PolymerElement, html } from '@polymer/polymer/polymer-element.js';
 import { setPassiveTouchGestures, setRootPath } from '@polymer/polymer/lib/utils/settings.js';
@@ -22,13 +13,11 @@ import '@polymer/iron-pages/iron-pages.js';
 import '@polymer/iron-selector/iron-selector.js';
 import '@polymer/paper-icon-button/paper-icon-button.js';
 
-
-// Gesture events like tap and track generated from touch will not be
-// preventable, allowing for better scrolling performance.
+//routing
 setPassiveTouchGestures(true);
 
-// Set Polymer's root path to the same value we passed to our service worker
-// in `index.html`.
+//routing path
+
 setRootPath(MyAppGlobals.rootPath);
 
 class MyApp extends PolymerElement {
@@ -44,7 +33,7 @@ class MyApp extends PolymerElement {
 
         app-drawer-layout:not([narrow]) [drawer-toggle] {
           display: none;
-          background-color: var(--app-primary-color);
+        
         }
 
         app-header {
@@ -112,13 +101,13 @@ class MyApp extends PolymerElement {
       </app-drawer-layout>
     `;
   }
-
+//properties
   static get properties() {
     return {
       page: {
         type: String,
         reflectToAttribute: true,
-        observer: '_pageChanged'
+        observer: 'routing'
       },
       routeData: Object,
       subroute: Object
@@ -131,26 +120,23 @@ class MyApp extends PolymerElement {
     ];
   }
 
+  //observer is method or function or callback
+
   _routePageChanged(page) {
-     // Show the corresponding page according to the route.
-     //
-     // If no page was found in the route data, page will be an empty string.
-     // Show 'view1' in that case. And if the page doesn't exist, show 'view404'.
+  
     if (!page) {
       this.page = 'loginpage';
     } else if (['loginpage','renewalspage','policy','new-policy'].indexOf(page) !== -1) {
       this.page = page;
-    } else {
-      this.page = 'view404';
     }
 
-    // Close a non-persistent drawer when the page & route are changed.
+  
     if (!this.$.drawer.persistent) {
       this.$.drawer.close();
     }
   }
 
-  _pageChanged(page) {
+  routing(page) {
     // Import the page component on demand.
     //
     // Note: `polymer build` doesn't like string concatenation in the import
